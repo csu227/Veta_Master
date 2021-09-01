8842a3b788a25538d45b97475207cc7bf4c57847
cs201470 <cs201470@ISSF3SI09WD.shepctr.org>
2021-09-01 16:18:58 -0400

    added mcd find as a function so mcd can be a variable

---
+++ Veta-master/MCD_Find.m
@@ -0,0 +1,9 @@
+function [upperbound,lowerbound,MCD] = MCD_Find(x)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+MCD = mean(abs(diff(x)));
+avg = mean(x);
+
+upperbound = avg + MCD * 2.66; % used to find offset
+lowerbound = avg - MCD * 2.66; %used to find onset
+end
